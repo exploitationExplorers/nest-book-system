7c46e54a40136e06fd731b91a07cefd7319880cb
fengzebing <1390653296@qq.com>
2025-02-11 19:33:04 +0800

    feat: login and register

--- book-management-system-frontend/src/main.tsx
+++ book-management-system-frontend/src/main.tsx
@@ -1,10 +1,29 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import {RouterProvider, createBrowserRouter} from 'react-router-dom'
 import './index.css'
-import App from './App.tsx'
+import {Login} from './pages/Login'
+import {Register} from './pages/Register'
+import {BookManage} from './pages/BookManage'
+
+const routes = [
+  {
+    path: "/login",
+    element: <Login/>,
+  },
+  {
+    path: "/register",
+    element: <Register/>,
+  },
+  {
+    path: "/",
+    element: <BookManage/>,
+  },
+];
+
+const router = createBrowserRouter(routes)
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+
+    <RouterProvider router={router} />
+
 )
---
+++ book-management-system-frontend/src/pages/BookManage/index.tsx
@@ -0,0 +1,7 @@
+export function BookManage() {
+  return (
+    <div>
+      <h1>BookManage</h1>
+    </div>
+  );
+}
