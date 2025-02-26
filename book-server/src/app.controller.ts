7800509f25fec763b79dc9013b881ca6ec8e8ff0
fengzebing <1390653296@qq.com>
2025-02-26 10:11:42 +0800

    :heavy_plus_sign: 添加nest/config

--- book-server/src/app.controller.ts
+++ book-server/src/app.controller.ts
@@ -1,19 +1,24 @@
 import { Controller, Get, Logger, Post, Body } from '@nestjs/common';
 import { AppService } from './app.service';
-
+import { ConfigService } from '@nestjs/config';
 @Controller()
 export class AppController {
   private logger = new Logger('AppController');
   constructor(private readonly appService: AppService) {}
-
+  @Inject(ConfigService)
+  private configService: ConfigService;
   @Get()
-  getHello(): string {
-    this.logger.log('Hello World');
-    this.logger.error('error', AppController.name);
-    this.logger.log('log', AppController.name);
-    this.logger.verbose('verbose', AppController.name);
-    this.logger.warn('warn', AppController.name);
-    return this.appService.getHello();
+  getHello() {
+    // this.logger.log('Hello World');
+    // this.logger.error('error', AppController.name);
+    // this.logger.log('log', AppController.name);
+    // this.logger.verbose('verbose', AppController.name);
+    // this.logger.warn('warn', AppController.name);
+    //return this.appService.getHello();
+    return {
+      a: this.configService.get('a'),
+      b: this.configService.get('b'),
+    };
   }
   @Post('log')
   log(@Body() body) {
--- book-server/src/app.module.ts
+++ book-server/src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
+import { ConfigModule } from '@nestjs/config';
 import { DbModule } from './db/db.module';
 import { BookModule } from './book/book.module';
 import { MyLogger } from './myLogger';
@@ -10,6 +11,7 @@ import { LogTestModule } from './log-test/log-test.module';
 import { DynamicLogger } from './dynamicLogger/dynamicLogger.moudle';
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     UserModule,
     DbModule,
     BookModule,
