c231ec5c68d19b537f6423d5ca73be55c27b62f8
fengzebing <1390653296@qq.com>
2025-04-01 21:32:18 +0800

    :construction: refresh token demo

---
+++ access_token_and_refresh_token/src/app.module.ts
@@ -0,0 +1,28 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './user/entities/user.entity';
+@Module({
+  imports: [
+    UserModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql', //数据库类型
+      username: 'root', //账号
+      password: 'asd.12345', //密码
+      host: 'localhost', //host
+      port: 3306, //
+      database: 'rbac_test', //库名
+      entities: [User],
+      synchronize: true, //synchronize字段代表是否自动将实体类同步到数据库
+      retryDelay: 500, //重试连接数据库间隔
+      retryAttempts: 10, //重试连接数据库的次数
+      logging: true, //是否开启日志
+      poolSize: 10, //连接池大小
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
