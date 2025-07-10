afacd1e0aa2edd25f315a3f6f9a1afdf7f69d775
fengzebing <1390653296@qq.com>
2025-07-10 20:52:30 +0800

    feat: 考试系统登录注册、登录鉴权、token自动续期

--- exam-system/apps/user/src/user.controller.ts
+++ exam-system/apps/user/src/user.controller.ts
@@ -8,11 +8,17 @@ import {
   Delete,
   Inject,
   Query,
+  SetMetadata,
+  BadRequestException,
 } from '@nestjs/common';
 import { UserService } from './user.service';
 import { RegisterUserDto } from './dto/register-user.dto';
 import { EmailService } from '@app/email';
 import { RedisService } from '@app/redis';
+import { LoginUserDto } from './dto/login-user.dto';
+import { JwtService } from '@nestjs/jwt';
+import { RequireLogin, UserInfo } from '@app/common';
+import { UpdateUserPasswordDto } from './dto/update-user-password.dto';
 
 @Controller('user')
 export class UserController {
@@ -41,5 +47,56 @@ export class UserController {
   @Post('register')
   async register(@Body() registerUser: RegisterUserDto) {
     return await this.userService.register(registerUser);
+  }
+  @Inject(JwtService)
+  private jwtService: JwtService;
+  @Post('login')
+  async userLogin(@Body() loginUser: LoginUserDto) {
+    const user = await this.userService.login(loginUser);
+
+    return {
+      user,
+      token: this.jwtService.sign(
+        {
+          userId: user.id,
+          username: user.username,
+        },
+        {
+          expiresIn: '7d',
+        },
+      ),
+    };
+  }
+
+  @Get('aaa')
+  @RequireLogin()
+  // aaa(@UserInfo() userInfo, @UserInfo('username') username) {
+  //   console.log(userInfo, username);
+  //   return 'aaa';
+  // }
+  @Post('update_password')
+  async updatePassword(@Body() passwordDto: UpdateUserPasswordDto) {
+    return this.userService.updatePassword(passwordDto);
+  }
+
+  @Get('update_password/captcha')
+  async updatePasswordCaptcha(@Query('address') address: string) {
+    if (!address) {
+      throw new BadRequestException('邮箱地址不能为空');
+    }
+    const code = Math.random().toString().slice(2, 8);
+
+    await this.redisService.set(
+      `update_password_captcha_${address}`,
+      code,
+      10 * 60,
+    );
+
+    await this.emailService.sendMail({
+      to: address,
+      subject: '更改密码验证码',
+      html: `<p>你的更改密码验证码是 ${code}</p>`,
+    });
+    return '发送成功';
   }
 }
--- exam-system/apps/user/src/user.module.ts
+++ exam-system/apps/user/src/user.module.ts
@@ -4,9 +4,34 @@ import { UserService } from './user.service';
 import { RedisModule } from '@app/redis';
 import { PrismaModule } from '@app/prisma';
 import { EmailModule } from '@app/email';
+import { JwtModule } from '@nestjs/jwt';
+import { APP_GUARD } from '@nestjs/core';
+import { AuthGuard } from '@app/common';
 @Module({
-  imports: [RedisModule, PrismaModule, EmailModule],
+  imports: [
+    RedisModule,
+    PrismaModule,
+    EmailModule,
+    JwtModule.registerAsync({
+      global: true,
+      useFactory() {
+        return {
+          secret: 'secretKey', // Replace with your actual secret key
+          signOptions: {
+            // Options for the token
+            expiresIn: '30m', // 30分钟
+          },
+        };
+      },
+    }),
+  ],
   controllers: [UserController],
-  providers: [UserService],
+  providers: [
+    UserService,
+    {
+      provide: APP_GUARD,
+      useClass: AuthGuard,
+    },
+  ],
 })
 export class UserModule {}
