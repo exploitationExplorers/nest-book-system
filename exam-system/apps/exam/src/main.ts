d322a27cddaeb6b110765201e7f5e1b182a11df1
fengzebing <1390653296@qq.com>
2025-07-12 14:44:39 +0800

    :sparkles:实现前后端登录注册联调

--- exam-system/apps/exam/src/main.ts
+++ exam-system/apps/exam/src/main.ts
@@ -1,19 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { ExamModule } from './exam.module';
-import { Transport } from '@nestjs/microservices';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(ExamModule);
 
-  app.connectMicroservice({
+  app.connectMicroservice<MicroserviceOptions>({
     transport: Transport.TCP,
     options: {
       port: 8888,
     },
   });
-  app.startAllMicroservices();
+
+  app.enableCors();
+
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
-  // await app.listen(process.env.port ?? 3002);
+
+  await app.listen(3002);
 }
 bootstrap();
