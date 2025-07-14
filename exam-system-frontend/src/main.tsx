2f95d57af373bd431b7f2821b445c40beb7b287c
fengzebing <1390653296@qq.com>
2025-07-14 11:06:02 +0800

    :sparkles: react-dnd实现试卷编辑器

--- exam-system-frontend/src/main.tsx
+++ exam-system-frontend/src/main.tsx
@@ -4,6 +4,9 @@ import { Register } from './pages/Register';
 import { Login } from './pages/Login';
 import { UpdatePassword } from './pages/UpdatePassword';
 import { ExamList } from './pages/ExamList';
+import { Edit } from './pages/Edit';
+import { DndProvider } from 'react-dnd';
+import { HTML5Backend } from 'react-dnd-html5-backend';
 
 const routes = [
   {
@@ -14,6 +17,8 @@ const routes = [
     path: "login",
     element: <Login />,
   },
+
+
   {
     path: "register",
     element: <Register />,
@@ -21,6 +26,10 @@ const routes = [
   {
     path: "update_password",
     element: <UpdatePassword />,
+  },
+  {
+    path: "edit/:id",
+    element: <Edit />
   }
 ];
 const router = createBrowserRouter(routes);
@@ -29,4 +38,6 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 
-root.render(<RouterProvider router={router}/>);
+root.render(<DndProvider backend={HTML5Backend}>
+  <RouterProvider router={router}/>
+</DndProvider>);
